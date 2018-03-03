c464b4c80c221acb208d74d8e7ab931bdd9b0da8
vincentmuya <vincentmuya13@gmail.com>
2018-03-03 11:47:28 +0300

    finished registration and login form

--- insta/gram/views.py
+++ insta/gram/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url='/accounts/login/')
+@login_required(login_url='/accounts/register/')
 def index(request):
     return render(request, "index.html")
