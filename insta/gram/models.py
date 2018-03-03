01e170125f7daf84d6a219411410046d66ebf62e
vincentmuya <vincentmuya13@gmail.com>
2018-03-03 13:42:33 +0300

    created user model

--- insta/gram/models.py
+++ insta/gram/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length = 30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user_name
+
+    def save_user(self):
+        self.save()
