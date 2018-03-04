0dcd9029169a7748ab72e0849f92dc80646793e4
vincentmuya <vincentmuya13@gmail.com>
2018-03-05 00:00:42 +0300

    added profile image to editor class

--- insta/gram/models.py
+++ insta/gram/models.py
@@ -5,6 +5,7 @@ from tinymce.models import HTMLField
 class Editor(models.Model):
     user_name = models.CharField(max_length = 30)
     email = models.EmailField()
+    profile_image = models.ImageField(upload_to="posts/",blank = True, null = True)
 
     def __str__(self):
         return self.user_name
